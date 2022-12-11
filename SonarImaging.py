6c8ed0f2f97920b4811750963a00271932c438b3
Ivan Anisimov <oceanbreak@gmail.com>
2022-12-11 21:24:36 +0300

    Added detect and descript section

--- SonarImaging.py
+++ SonarImaging.py
@@ -98,6 +98,63 @@ def sliceImage(image, ptA, ptB):
     return (values, rr, cc)
 
 
+############# Detection and descriptpors section #################
+
+def detectKeypoints(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()
+    kps, descript = sift.detectAndCompute(gray, None)
+    kps = np.float32([kp.pt for kp in kps])
+    return (kps, descript)
+
+def matchKeypoints(des1, des2, ratio):
+    # print('matchKeypoints')
+    matcher = cv2.DescriptorMatcher_create('BruteForce')
+    try:
+        rawMatches = matcher.knnMatch(des1, des2, 2)
+    except:
+        return None
+
+    # Lowe's ratio test
+    matches = []
+    for m in rawMatches:
+        if len(m) ==2 and m[0].distance < m[1].distance * ratio:
+            matches.append((m[0].trainIdx, m[0].queryIdx))
+    if len(matches) > 4:
+        return np.array(matches)
+    else:
+        return None
+
+def estimateInliers(matches, kpsA, kpsB, reprojThresh):
+    if matches is not None:
+        ptsA = np.float32([kpsA[i] for i in matches[:,1]])
+        ptsB = np.float32([kpsB[i] for i in matches[:,0]])
+        (H, status) = cv2.findHomography(ptsB, ptsA, cv2.RANSAC, reprojThresh)
+        return (matches, H, status)
+    else:
+        return None
+
+def drawMatches(imageA, imageB, kpsA, kpsB, matches, status):
+    # initialize the output visualization image
+    (hA, wA) = imageA.shape[:2]
+    (hB, wB) = imageB.shape[:2]
+    vis = np.zeros((max(hA, hB), wA + wB, 3), dtype="uint8")
+    vis[0:hA, 0:wA] = imageA
+    vis[0:hB, wA:] = imageB
+    # loop over the matches
+    for ((trainIdx, queryIdx), s) in zip(matches, status):
+        # only process the match if the keypoint was successfully
+        # matched
+        if s == 1:
+            # draw the match
+            ptA = (int(kpsA[queryIdx][0]), int(kpsA[queryIdx][1]))
+            ptB = (int(kpsB[trainIdx][0]) + wA, int(kpsB[trainIdx][1]))
+            cv2.line(vis, ptA, ptB, (0, 255, 0), 1)
+    # return the visualization
+    return vis
+
+####################################################
+
 ### CLASSES ###
 class VideoPlayer:
     """
